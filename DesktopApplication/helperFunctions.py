fb842cf16c5780cc51eed7bfc02e22d1e6579633
jschlecht <j.schlecht@inovex.de>
2023-12-17 11:53:53 +0100

    adds programm support for creating and using model

---
+++ DesktopApplication/helperFunctions.py
@@ -0,0 +1,58 @@
+import numpy as np
+import tensorflow as tf
+import pretty_midi
+import collections
+import pandas as pd
+import keras
+
+
+def midi_to_notes(midi_file: str) -> pd.DataFrame:
+        pm = pretty_midi.PrettyMIDI(midi_file)
+        instrument = pm.instruments[0]
+        notes = collections.defaultdict(list)
+
+        # Sort the notes by start time
+        sorted_notes = sorted(instrument.notes, key=lambda note: note.start)
+        prev_start = sorted_notes[0].start
+
+        for note in sorted_notes:
+            start = note.start
+            end = note.end
+            notes['pitch'].append(note.pitch)
+            notes['start'].append(start)
+            notes['end'].append(end)
+            notes['step'].append(start - prev_start)
+            notes['duration'].append(end - start)
+            prev_start = start
+
+        return pd.DataFrame({name: np.array(value) for name, value in notes.items()})
+
+def notes_to_midi(notes: pd.DataFrame, out_file: str, instrument_name: str, velocity: int = 100) -> pretty_midi.PrettyMIDI:
+
+    pm = pretty_midi.PrettyMIDI()
+    instrument = pretty_midi.Instrument(
+        program=pretty_midi.instrument_name_to_program(
+            instrument_name))
+
+    prev_start = 0
+    for i, note in notes.iterrows():
+        start = float(prev_start + note['step'])
+        end = float(start + note['duration'])
+        note = pretty_midi.Note(
+            velocity=velocity,
+            pitch=int(note['pitch']),
+            start=start,
+            end=end,
+        )
+        instrument.notes.append(note)
+        prev_start = start
+
+    pm.instruments.append(instrument)
+    pm.write(out_file)
+    return pm
+
+@keras.saving.register_keras_serializable()
+def mse_with_positive_pressure(y_true: tf.Tensor, y_pred: tf.Tensor):
+  mse = (y_true - y_pred) ** 2
+  positive_pressure = 10 * tf.maximum(-y_pred, 0.0)
+  return tf.reduce_mean(mse + positive_pressure)

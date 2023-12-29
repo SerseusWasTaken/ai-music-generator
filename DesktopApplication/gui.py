95343cfb725ef55cfe63ddf6f1446cfbb13250cf
jschlecht <j.schlecht@inovex.de>
2023-12-29 11:24:58 +0100

    add music generation to gui

--- DesktopApplication/gui.py
+++ DesktopApplication/gui.py
@@ -1,6 +1,29 @@
 import tkinter as tk
 from tkinter import ttk
+#Music generation related
+from helperFunctions import mse_with_positive_pressure
+import keras
+import tensorflow as tf
+from modelAPI import MusicRNN
+import pathlib
+import glob
 
+def generateMusicFile():
+    model = tf.keras.saving.load_model('models/tuner_best_mode.keras')
+    modelAPI = MusicRNN(model, 2.0, 128)
+
+    data_dir = pathlib.Path('data/maestro-v3.0.0')
+    if not data_dir.exists():
+        tf.keras.utils.get_file(
+            'maestro-v3.0.0-midi.zip',
+            origin='https://storage.googleapis.com/magentadata/datasets/maestro/v3.0.0/maestro-v3.0.0-midi.zip',
+            extract=True,
+            cache_dir='.', cache_subdir='data',
+        )
+    filenames = glob.glob(str(data_dir/'**/*.mid*'))
+    sample_file = filenames[1]
+
+    modelAPI.createMusicFile(sample_file)
 
 # creates Window
 root = tk.Tk()
@@ -37,7 +60,8 @@ devNames.pack()
 
 button = ttk.Button(
     root,
-    text="Create a masterpiece"
+    text="Create a masterpiece",
+    command=lambda: generateMusicFile()
 )
 button.pack()
 
